a375c602f3d46c4dfd4856935dae89b9c35985a9
Dzmitry Nikalayeu <dzmitry.nikalayeu@scania.com>
2026-03-19 14:49:51 +0100

    ci: deploy app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: process.env.GITHUB_ACTIONS ? "/Scania_driver_app/" : "/",
   server: {
     host: "::",
     port: 8080,
